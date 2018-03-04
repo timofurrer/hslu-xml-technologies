199fe6586c12d767775f479fe14e14fb05fc97c2
Timo Furrer <tuxtimo@gmail.com>
2018-03-04 13:35:52 +0100

    Split files

--- app/index.php
+++ app/index.php
@@ -45,23 +45,7 @@
                 <div class="row">
                     <div class="col-md-12">
 <?php
-
-$page = isset($_GET['page']) ? trim($_GET['page']) : 'offers';
-
-// Hello security
-$input_data = file_get_contents('xml/'.$page.'.xml');
-$xml = new DOMDocument();
-$xml->loadXML($input_data);
-
-$xsl = new DOMDocument();
-$xsl->load('xsl/'.$page.'.xsl');
-
-// transform
-$xslt_proc = new XSLTProcessor();
-$xslt_proc->importStylesheet($xsl);
-$dom = $xslt_proc->transformToDoc($xml);
-
-echo $dom->saveXML();
+    require 'load_content.php';
 ?>
                     </div>
                 </div>
