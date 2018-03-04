7e9966f9ae851d7f0a442024ba66a973e26e7225
Timo Furrer <tuxtimo@gmail.com>
2018-03-04 13:41:50 +0100

    Improve

--- app/load_content.php
+++ app/load_content.php
@@ -1,18 +1,30 @@
 <?php
+
+function load_page($xml_path, $xsl_path) {
+    // Hello security
+    $input_data = file_get_contents($xml_path);
+    $xml = new DOMDocument();
+    $xml->loadXML($input_data);
+
+    $xsl = new DOMDocument();
+    $xsl->load($xsl_path);
+
+    // transform
+    $xslt_proc = new XSLTProcessor();
+    $xslt_proc->importStylesheet($xsl);
+    $dom = $xslt_proc->transformToDoc($xml);
+
+    return $dom->saveXML();
+}
+
 $page = isset($_GET['page']) ? trim($_GET['page']) : 'offers';
 
-// Hello security
-$input_data = file_get_contents('xml/'.$page.'.xml');
-$xml = new DOMDocument();
-$xml->loadXML($input_data);
+switch($page) {
+    case 'offers':
+        echo load_page('xml/'.$page.'.xml', 'xsl/'.$page.'.xsl');
+        break;
+    default:
+        die('Yet undefined page ' . $page);
+}
 
-$xsl = new DOMDocument();
-$xsl->load('xsl/'.$page.'.xsl');
-
-// transform
-$xslt_proc = new XSLTProcessor();
-$xslt_proc->importStylesheet($xsl);
-$dom = $xslt_proc->transformToDoc($xml);
-
-echo $dom->saveXML();
 ?>
