e46b2c8624e36f5d3e8ce855a6c38a25690365f7
Timo Furrer <tuxtimo@gmail.com>
2018-03-10 13:29:58 +0100

    Basic registration

--- app/registration.php
+++ app/registration.php
@@ -5,41 +5,40 @@ function register($eventId, $name, $email, $tel, $address, $with_stayover, $rest
 
     // create new Registration entry
     $reg_node = $xml->addChild('registration');
-    $reg_node->addChild('eventId', $eventId);
+    $reg_node->addChild('eventId', htmlentities($eventId));
     $participant_node = $reg_node->addChild('participant');
-    $participant_node->addChild('name', $name);
-    $participant_node->addChild('email', $email);
-    $participant_node->addChild('tel', $tel);
-    $participant_node->addChild('address', $address);
-    $reg_node->addChild('needs_stayover', $with_stayover);
+    $participant_node->addChild('name', htmlentities($name));
+    $participant_node->addChild('email', htmlentities($email));
+    $participant_node->addChild('tel', htmlentities($tel));
+    $participant_node->addChild('address', htmlentities($address));
+    $reg_node->addChild('needs_stayover', htmlentities($with_stayover));
     $restriction_node = $reg_node->addChild('restrictions');
     foreach($restrictions as $restriction) {
-        $restriction_node->addChild('restriction', $restriction);
+        $restriction_node->addChild('restriction', htmlentities($restriction));
     }
-    $restriction_node->addChild('description', $description);
-
+    $restriction_node->addChild('description', htmlentities($description));
     file_put_contents('xml/registrations.xml', $xml->asXML());
+
+    echo "<div class=\"alert alert-success\" role=\"alert\">Sie wurden erfolgreich angemeldet!</div>";
 }
 
 function verify($available_fields, $required_fields) {
     # check required fields
     foreach($required_fields as $field) {
         if(empty($available_fields[$field])) {
-            echo "Bitte das Feld $field angeben";
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Bitte das Feld $field angeben</div>";
             return false;
         }
     }
     return true;
 }
 
-var_dump($_POST);
-
 if(!empty($_POST)) {
     $required_fields = array('name', 'email', 'tel', 'address');
 
     if(verify($_POST, $required_fields)) {
         register($_GET['event'], $_POST['name'], $_POST['email'], $_POST['tel'], $_POST['address'],
-            isset($_POST['uebernachtung']), $_POST['restrictions'], $_POST['description']);
+            isset($_POST['uebernachtung']), $_POST['restrictions'], $_POST['restriction_description']);
     }
 }
 ?>
