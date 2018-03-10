e096078de7de161975cd0fb29533d49a24f0da96
Timo Furrer <tuxtimo@gmail.com>
2018-03-10 16:00:45 +0100

    Add XSD validation for registration

--- app/registration.php
+++ app/registration.php
@@ -16,21 +16,45 @@ function register($eventId, $name, $email, $tel, $address, $with_stayover, $rest
     $participant_node->addChild('email', htmlentities($email));
     $participant_node->addChild('tel', htmlentities($tel));
     $participant_node->addChild('address', htmlentities($address));
-    $reg_node->addChild('needs_stayover', htmlentities($with_stayover));
+    $reg_node->addChild('needs_stayover', htmlentities((string)$with_stayover));
     $restriction_node = $reg_node->addChild('restrictions');
     foreach($restrictions as $restriction) {
         $restriction_node->addChild('restriction', htmlentities($restriction));
     }
     $restriction_node->addChild('description', htmlentities($description));
-    file_put_contents('xml/registrations.xml', $xml->asXML());
 
-    // generate confirmation PDF
-    $confirmation = generate_confirmation($registration_id);
+    $to_persist_xml = new DOMDocument();
+    $to_persist_xml->formatOutput = true;
+    $to_persist_xml->preserveWhiteSpace = false;
+    $to_persist_xml->loadXML($xml->asXML());
 
-    echo '<div class="alert alert-success" role="alert">';
-    echo "Sie wurden erfolgreich angemeldet! ";
-    echo $confirmation;
-    echo "</div>";
+    // validate XML before persisting
+    // disable error output to client
+    libxml_use_internal_errors(true);
+    $result = $to_persist_xml->schemaValidate('xsd/registrations.xsd');
+
+    if(!$result) {
+        echo '<div class="alert alert-danger" role="alert">';
+        // schema validation failed
+        $errors = libxml_get_errors();
+        foreach($errors as $error) {
+            echo sprintf('Line [%d]: %s', $error->line, $error->message);
+        }
+        libxml_clear_errors();
+
+        echo "</div>";
+    }
+    else {
+        file_put_contents('xml/registrations.xml', $to_persist_xml->saveXML());
+
+        // generate confirmation PDF
+        $confirmation = generate_confirmation($registration_id);
+
+        echo '<div class="alert alert-success" role="alert">';
+        echo "Sie wurden erfolgreich angemeldet! ";
+        echo $confirmation;
+        echo "</div>";
+    }
 }
 
 function verify($available_fields, $required_fields) {
@@ -49,7 +73,7 @@ if(!empty($_POST)) {
 
     if(verify($_POST, $required_fields)) {
         register($_GET['event'], $_POST['name'], $_POST['email'], $_POST['tel'], $_POST['address'],
-            isset($_POST['uebernachtung']), $_POST['restrictions'], $_POST['restriction_description']);
+            $_POST['uebernachtung'] ? 'true' : 'false', $_POST['restrictions'], $_POST['restriction_description']);
     }
 }
 ?>
